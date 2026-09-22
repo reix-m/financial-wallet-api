90a0fa029be8607d95072c3caa3fcf466a67d570
Maycon Reis <reix-m@pm.me>
2026-09-22 11:58:29 -0300

    style(config): format config files

---
+++ config/scribe.php
@@ -0,0 +1,116 @@
+<?php
+
+declare(strict_types=1);
+
+use Knuckles\Scribe\Config\AuthIn;
+use Knuckles\Scribe\Config\Defaults;
+
+use function Knuckles\Scribe\Config\removeStrategies;
+
+use Knuckles\Scribe\Extracting\Strategies;
+
+return [
+    'title' => config('app.name') . ' API Documentation',
+    'description' => 'Uma interface funcional equivalente a uma carteira financeira em que os usuários possam realizar transferência de saldo e depósito.',
+    'base_url' => config('app.url'),
+    'routes' => [
+        [
+            'match' => [
+                'prefixes' => ['api/*'],
+                'domains' => ['*'],
+            ],
+            'include' => [
+            ],
+            'exclude' => [
+            ],
+        ],
+    ],
+
+    'type' => 'laravel',
+    'theme' => 'default',
+    'static' => [
+        'output_path' => 'public/docs',
+    ],
+    'laravel' => [
+        'add_routes' => true,
+        'docs_url' => '/docs',
+        'assets_directory' => null,
+        'middleware' => [],
+    ],
+    'external' => [
+        'html_attributes' => [],
+    ],
+    'try_it_out' => [
+        'enabled' => true,
+        'base_url' => null,
+        'use_csrf' => false,
+        'csrf_url' => '/sanctum/csrf-cookie',
+    ],
+    'auth' => [
+        'enabled' => false,
+        'default' => false,
+        'in' => AuthIn::BEARER->value,
+        'name' => 'key',
+        'use_value' => env('SCRIBE_AUTH_KEY'),
+        'placeholder' => '{YOUR_AUTH_KEY}',
+        'extra_info' => 'You can retrieve your token by visiting your dashboard and clicking <b>Generate API token</b>.',
+    ],
+    'example_languages' => [
+        'bash',
+        'javascript',
+    ],
+    'postman' => [
+        'enabled' => true,
+        'overrides' => [
+        ],
+    ],
+    'openapi' => [
+        'enabled' => true,
+        'version' => '3.0.3',
+        'overrides' => [
+        ],
+        'generators' => [],
+    ],
+    'groups' => [
+        'default' => 'Endpoints',
+        'order' => [],
+    ],
+    'logo' => false,
+    'last_updated' => 'Last updated: {date:F j, Y}',
+    'examples' => [
+        'faker_seed' => 1234,
+        'models_source' => ['factoryCreate', 'factoryMake', 'databaseFirst'],
+    ],
+    'strategies' => [
+        'metadata' => [
+            ...Defaults::METADATA_STRATEGIES,
+        ],
+        'headers' => [
+            ...Defaults::HEADERS_STRATEGIES,
+            Strategies\StaticData::withSettings(data: [
+                'Content-Type' => 'application/json',
+                'Accept' => 'application/json',
+            ]),
+        ],
+        'urlParameters' => [
+            ...Defaults::URL_PARAMETERS_STRATEGIES,
+        ],
+        'queryParameters' => [
+            ...Defaults::QUERY_PARAMETERS_STRATEGIES,
+        ],
+        'bodyParameters' => [
+            ...Defaults::BODY_PARAMETERS_STRATEGIES,
+        ],
+        'responses' => removeStrategies(
+            Defaults::RESPONSES_STRATEGIES,
+            [Strategies\Responses\ResponseCalls::class],
+        ),
+        'responseFields' => [
+            ...Defaults::RESPONSE_FIELDS_STRATEGIES,
+        ],
+    ],
+    'database_connections_to_transact' => [config('database.default')],
+    'fractal' => [
+        'serializer' => null,
+    ],
+];
