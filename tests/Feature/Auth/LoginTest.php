8439960a22f33f7ba1f25b71eca09c9a719a5a8e
Maycon Reis <reix-m@pm.me>
2026-09-22 20:13:05 -0300

    test(auth): adds login endpoint tests

---
+++ tests/Feature/Auth/LoginTest.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(RefreshDatabase::class);
+
+it('cannot authenticate with invalid email', function (): void {
+    $response = $this->postJson('/api/v1/auth/login', [
+        'email' => 'foo.bar@example.com',
+        'password' => 'StrongP@ss123',
+    ]);
+
+    $response->assertUnauthorized();
+});
+
+it('cannot authenticate with invalid password', function (): void {
+    $user = User::factory()->create([
+        'email' => 'foo.bar@example.com',
+        'password' => 'StrongP@ss123'
+    ]);
+    $response = $this->postJson('/api/v1/auth/login', [
+        'email' => $user->email,
+        'password' => 'InvalidPass',
+    ]);
+
+    $response->assertUnauthorized();
+});
+
+it('can authenticate with valid email and password', function (): void
+{
+    $user = User::factory()->create([
+        'email' => 'foo.bar@example.com',
+        'password' => 'StrongP@ss123'
+    ]);
+
+    $response = $this->postJson('/api/v1/auth/login', [
+        'email' => $user->email,
+        'password' => 'StrongP@ss123',
+    ]);
+
+    $response
+        ->assertOk()
+        ->assertJsonStructure(['user' => ['id', 'name', 'email'], 'access_token', 'token_type', 'expires_at'])
+        ->assertJsonPath('token_type', 'Bearer')
+        ->assertJsonPath('user.email', 'foo.bar@example.com');
+
+});
