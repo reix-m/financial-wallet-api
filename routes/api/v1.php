bcf60e1258471cd1aae1314c3379feeed532a90a
Maycon Reis <reix-m@pm.me>
2026-09-22 20:12:39 -0300

    feat(auth) : adds login endpoint

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -6,8 +6,9 @@ use App\Http\Controllers\Api\V1\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('auth')->controller(AuthController::class)->group(function (): void {
-    Route::post('/register', 'register')->name('v1.auth.register');
+    Route::post('/register', 'register')->middleware('throttle:auth-register')->name('v1.auth.register');
     Route::get('/email/verify/{id}/{hash}', 'verifyEmail')
         ->name('verification.verify')
         ->middleware(['signed', 'throttle:6,1']);
+    Route::post('/login', 'login')->middleware('throttle:auth-login')->name('v1.auth.login');
 });
