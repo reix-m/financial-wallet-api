7cbad874e8e206063d206afe51095bafa8969485
Maycon Reis <reix-m@pm.me>
2026-09-23 19:51:05 -0300

    feat(wallet): adds list transactions action

---
+++ app/Actions/Wallet/ListTransactions/ListTransactions.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Actions\Wallet\ListTransactions;
+
+use App\Domain\Wallet\Exceptions\WalletNotFoundException;
+use App\Models\Transaction;
+use App\Models\Wallet;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+
+final class ListTransactions
+{
+    /**
+     * @return LengthAwarePaginator<int, Transaction>
+     */
+    public function execute(ListTransactionsInput $input): LengthAwarePaginator
+    {
+        /** @var ?Wallet $wallet */
+        $wallet = Wallet::query()->where('user_id', $input->userId)->first();
+
+        if ( ! $wallet) {
+            throw new WalletNotFoundException();
+        }
+
+        return Transaction::query()
+            ->where('wallet_id', $wallet->id)
+            ->with([
+                'counterpartyWallet' => function ($query): void {
+                    $query->select('wallets.id', 'user_id', 'code')
+                        ->with(['user' => function ($userQuery): void {
+                            $userQuery->select('id', 'name', 'email');
+                        }]);
+                },
+            ])
+            ->orderBy('created_at', 'desc')
+            ->paginate($input->perPage);
+    }
+}
