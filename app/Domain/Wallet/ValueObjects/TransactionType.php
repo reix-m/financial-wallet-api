00c54fd42cc23de7ca664aac75d369d643523ed6
Maycon Reis <reix-m@pm.me>
2026-09-23 17:28:12 -0300

    feat(wallet): adds transfer_in and transfer_out transaction types

--- app/Domain/Wallet/ValueObjects/TransactionType.php
+++ app/Domain/Wallet/ValueObjects/TransactionType.php
@@ -7,12 +7,14 @@ namespace App\Domain\Wallet\ValueObjects;
 enum TransactionType: string
 {
     case DEPOSIT = 'deposit';
+    case TRANSFER_OUT = 'transfer_out';
+    case TRANSFER_IN = 'transfer_in';
     case REVERSAL = 'reversal';
 
     public function isCredit(): bool
     {
         return match ($this) {
-            self::DEPOSIT => true,
+            self::DEPOSIT, self::TRANSFER_IN => true,
             default => false,
         };
     }
