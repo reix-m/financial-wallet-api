dd0f45b50066ad85ebc9765f0482f06b722da250
Maycon Reis <reix-m@pm.me>
2026-09-23 10:24:02 -0300

    feat(wallet): adds wallet domain entity

---
+++ app/Domain/Wallet/Wallet.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Wallet;
+
+use App\Domain\Wallet\Contracts\WalletCodeGeneratorInterface;
+use App\Domain\Wallet\ValueObjects\Money;
+use App\Domain\Wallet\ValueObjects\WalletCode;
+use Illuminate\Support\Carbon;
+use InvalidArgumentException;
+
+final class Wallet
+{
+    private ?int $id;
+    private WalletCode $code;
+    private int $userId;
+    private Carbon $createdAt;
+    private Money $balance;
+
+    public function __construct(WalletCode $code, int $userId, ?Money $balance, ?int $id = null, ?Carbon $createdAt = null)
+    {
+        if (empty($userId)) {
+            throw new InvalidArgumentException('The user id is required.');
+        }
+
+        $this->code = $code;
+        $this->id = $id;
+        $this->userId = $userId;
+        $this->balance = $balance ?? Money::zero();
+        $this->createdAt = $createdAt ?? now();
+    }
+
+    public static function create(int $userId, WalletCodeGeneratorInterface $codeGenerator, ?Money $initialBalance = null): self
+    {
+        return new self(
+            userId: $userId,
+            code: $codeGenerator->generate(),
+            balance: $initialBalance,
+        );
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCode(): WalletCode
+    {
+        return $this->code;
+    }
+
+    public function getUserId(): int
+    {
+        return $this->userId;
+    }
+
+    public function getBalance(): Money
+    {
+        return $this->balance;
+    }
+
+    public function getCreatedAt(): Carbon
+    {
+        return $this->createdAt;
+    }
+
+    public function setId(int $id): void
+    {
+        if (null !== $this->id) {
+            throw new InvalidArgumentException('The wallet id is already assigned.');
+        }
+
+        $this->id = $id;
+    }
+}
