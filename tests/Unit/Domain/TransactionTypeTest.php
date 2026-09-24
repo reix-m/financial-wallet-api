767df5031f271e12799ea53d4ac5b656e56d135f
Maycon Reis <reix-m@pm.me>
2026-09-24 00:51:57 -0300

    test(wallet): adds is transfer method tests

--- tests/Unit/Domain/TransactionTypeTest.php
+++ tests/Unit/Domain/TransactionTypeTest.php
@@ -1,6 +1,7 @@
 <?php
 
 declare(strict_types=1);
+
 use App\Domain\Wallet\ValueObjects\TransactionType;
 
 it('should return true when type is credit', function (): void {
@@ -9,4 +10,14 @@ it('should return true when type is credit', function (): void {
 
 it('should return false when type is not credit', function (): void {
     expect(TransactionType::REVERSAL->isCredit())->toBeFalse();
+});
+
+it('should return true when type is transfer', function (): void {
+    expect(TransactionType::TRANSFER_OUT->isTransfer())->toBeTrue();
+    expect(TransactionType::TRANSFER_IN->isTransfer())->toBeTrue();
+});
+
+it('should return false when type is not transfer', function (): void {
+    expect(TransactionType::DEPOSIT->isTransfer())->toBeFalse();
+    expect(TransactionType::REVERSAL->isTransfer())->toBeFalse();
 });
